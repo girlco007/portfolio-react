cf43b646b10a3e05ed9127f561fd56133e6bcb9d
angie <angelika.gaszczyk@gmail.com>
2017-03-07 23:22:38 -0800

    add moda page

--- src/projects/Moda.js
+++ src/projects/Moda.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import '../App.css';
-import gv from '../img/gv.png'
-import gvSpec from '../img/gvSpec.svg'
+import Img1 from '../img/modaStore/Img1.jpg';
+import Img2 from '../img/modaStore/Img2.jpg';
+import Img3 from '../img/modaStore/Img3.jpg';
+import Img4 from '../img/modaStore/Img4.jpg';
+import baner from '../img/modaStore/baner.png';
+import icon from '../img/modaStore/icon.png';
+import login from '../img/modaStore/login.png';
+import work from '../img/modaStore/work.png';
 
-import {Grid, Row, Col, Image} from 'react-bootstrap'
+
+
+
+
+import {Grid, Row, Col, Image, ResponsiveEmbed} from 'react-bootstrap'
 
 
 export class Moda extends React.Component {
@@ -16,20 +26,94 @@ export class Moda extends React.Component {
             <Col xs={7} xsOffset={1}>
               <h2>MODA+</h2>
               <h4>Mobile App</h4>
-
-              <p>Role - Designer</p>
-              <p>something</p>
+              <Row>
+                <Col xs={1}>
+                  <p>Role</p>
+                </Col>
+                <Col xs={3}>
+                  <p>Designer</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={1}>
+                  <p>Link</p>
+                </Col>
+                <Col xs={3}>
+                  <a href="https://itunes.apple.com/us/app/moda-plus/id1148484782?mt=8" target="_blank">Download Moda+ App</a>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={9}>
+                  <p>MODA+ is a mobile fashion app for individuals to showcase, inspire, and explore work within the growing fashion community. </p>
+                </Col>
+              </Row>
             </Col>
           </Row>
+
           <Row>
-            <Col md={2} mdOffset={1} xsHidden>
-              <Image src={gvSpec} alt='gumvelope spec' responsive className="margin"/>
+            <Col md={12}>
+              <Image src={baner} responsive/>
             </Col>
-            <Col xs={8} mdOffset={1}>
-               <Image src={gv} alt="gunvelope site" responsive className='shadow' />
+          </Row>
+
+          <Row>
+            <Col xs={6} xsOffset={2}>
+              <p>The goal for this design was to clean up the existing visual aesthetics. Since the main functions of the app are very similar to others, the challenge was to make it feel different. Also, the developer needed a simple design that could be built in two months max. The app is now in the app store for free download with these designs.</p>
+            </Col>
+            <Col xs={2}>
+              <Image src={icon} alt='moda-plus icon' responsive/>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={12}>
+              <h3>Login/Sing Up Screens</h3>
+            </Col>
+            <Col md={8} mdOffset={2}>
+              <Image src={login} alt='login and singup screens' responsive/>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={12}>
+              <h3>Working</h3>
+            </Col>
+            <Col md={10} mdOffset={1}>
+              <Image src={work} alt='eork example' responsive/>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={12}>
+              <h3>iOS App Store Screenshots</h3>
+            </Col>
+            <Col md={3} sm={6}>
+              <Image src={Img1} alt='gumvelope spec' responsive className='StoreShoots' />
+            </Col>
+            <Col md={3} sm={6}>
+               <Image src={Img2} alt="gunvelope site" responsive className='StoreShoots'  />
+            </Col>
+            <Col md={3} sm={6}>
+              <Image src={Img3} alt='gumvelope spec' responsive className='StoreShoots'/>
+            </Col>
+            <Col md={3} sm={6}>
+               <Image src={Img4} alt="gunvelope site" responsive className='StoreShoots'  />
             </Col>
           </Row>
         </Grid>
+
+        <Row>
+          <Col md={6} mdOffset={3}>
+            <div style={{width: 660, height: 'auto'}}>
+              <ResponsiveEmbed a16by9>
+                <embed type="image/svg+xml" src="https://www.youtube.com/embed/G8-XeHB-B6M" />
+              </ResponsiveEmbed>
+            </div>
+
+          </Col>
+        </Row>
+
+
       </div>
     );
   }
